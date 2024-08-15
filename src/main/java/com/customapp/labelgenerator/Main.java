71e2c5560027ea9c1ec38766e9774e7a5cc8b7bc
thuyn789 <thuynh8668@gmail.com>
2024-08-14 21:52:57 -0400

    Moved main function to its own class

---
+++ src/main/java/com/customapp/labelgenerator/Main.java
@@ -0,0 +1,7 @@
+package com.customapp.labelgenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        MainAppUI.launch(MainAppUI.class, args);
+    }
+}
--- src/main/java/com/customapp/labelgenerator/MainAppUI.java
+++ src/main/java/com/customapp/labelgenerator/MainAppUI.java
@@ -7,17 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainApplication extends Application {
+public class MainAppUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainAppUI.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 530, 350);
         stage.setTitle("Label Generator");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
